b212968c9fd2e7d011ed279add94265462f9e4b9
Yago Santos <yagodezoito@gmail.com>
2026-09-03 09:43:10 -0300

    First commit

---
+++ src/test/java/br/com/yago/apilojavirtual/LojaVitualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yago.apilojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVitualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
